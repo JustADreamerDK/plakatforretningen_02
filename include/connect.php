6b6562e629cd78fa2bf5708529c5a86bb4da4d97
Katja Kjer <Kalle3008@hotmail.com>
2019-09-11 08:48:12 +0200

    .

--- include/connect.php
+++ include/connect.php
@@ -1,6 +1,6 @@
 <?php
-// $objCon = new mysqli("localhost", "kkjer_dk_plakatforretningen", "eal010393", "kkjer_dk_plakatforretningen");
-$objCon = new mysqli("localhost", "root", "root", "plakatforretningen");
+$objCon = new mysqli("localhost", "kkjer_dk_plakatforretningen_02", "ucl010393", "kkjer_dk_plakatforretningen_02");
+// $objCon = new mysqli("localhost", "root", "root", "plakatforretningen");
 if ($objCon->connect_errno) {
     die('Kan ikke forbinde (' . $objCon->connect_errno . ')' . $objCon->connect_error);
 }
--- index.php
+++ index.php
@@ -54,10 +54,12 @@ $indlaeg = getIndlaeg();
                 if ($erDer <> ''){ ?>
                     <img class="image" src="img/<?php echo $erDer; ?>">
                 <?php }; ?>
+                
                 <?php $beskrivelse = mb_substr($rowIndlaeg['tekst'], 0, 500, 'UTF-8');
                 $tal = mb_strrpos($beskrivelse, ' ', 0, 'UTF-8');
                 $beskrivelse = mb_substr($rowIndlaeg['tekst'], 0, $tal, 'UTF-8'); ?>
                 <p class="m-tb-10 m-lr-20 computerText"><?php echo $beskrivelse . '...' ?> </p>
+
                 <?php $beskrivelse2 = mb_substr($rowIndlaeg['tekst'], 0, 200, 'UTF-8');
                 $tal2 = mb_strrpos($beskrivelse2, ' ', 0, 'UTF-8');
                 $beskrivelse2 = mb_substr($rowIndlaeg['tekst'], 0, $tal2, 'UTF-8'); ?>
